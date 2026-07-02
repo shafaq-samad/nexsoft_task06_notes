eb44b047179626687e3ee47eb669c581dc71046d
shafaq-samad <shafaqsamad111@gmail.com>
2026-07-02 23:11:45 +0500

    duplication issue

--- server.ts
+++ server.ts
@@ -27,7 +27,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
